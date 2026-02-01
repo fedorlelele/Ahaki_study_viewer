677058b29180a061252375ede7a53ac628847e8f
西谷拓朗 <nishitani@MacBook-Pro.local>
2026-02-01 16:35:56 +0900

    更新情報を追加

--- docs/web_app/config.js
+++ docs/web_app/config.js
@@ -1,2 +1,4 @@
 window.SUPABASE_URL = "https://fnidnxgliwclhmvovkji.supabase.co";
 window.SUPABASE_KEY = "sb_publishable_DJCGkRg8OSddT_fG6sp4vw_SZFQ9F1v";
+window.ADMIN_API_BASE = "https://ahaki-api.fedorp4pdk.workers.dev";
+window.AI_API_BASE = "https://ahaki-api.fedorp4pdk.workers.dev";
